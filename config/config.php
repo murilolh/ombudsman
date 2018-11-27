a7cd960c9bcf3637e583d3f4a4e23ccce763da99
Murilo Holanda <murilolh@gmail.com>
2018-11-27 20:12:05 -0200

    Changing config.php for Heroky deploy

--- config/config.php
+++ config/config.php
@@ -1,15 +1,15 @@
 <?php
 
-define('DB_NAME', 'ombudsman');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost');
+define('DB_NAME', getenv('OMBUD_DB_NAME'));
+define('DB_USER', getenv('OMBUD_DB_USER'));
+define('DB_PASSWORD', getenv('OMBUD_DB_PASSWORD'));
+define('DB_HOST', getenv('OMBUD_DB_HOST'));
 
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
 if ( !defined('BASEURL') )
-	define('BASEURL', '/ombudsman/');
+	define('BASEURL', getenv('OMBUD_BASE_URL'));
 
 if ( !defined('SYSTEMNAME') )
 	define('SYSTEMNAME', 'Ombudsman');
@@ -17,23 +17,23 @@ if ( !defined('SYSTEMNAME') )
 if ( !defined('DBAPI') )
 	define('DBAPI', ABSPATH . '../inc/database.php');
 
-define('SEND_EMAILS', false);
-define('SMTP_HOST', '');
-define('SMTP_PORT', '');
-define('SMTP_USER', '');
-define('SMTP_PASS', '');
-define('SMTP_USERNAME', 'Ombudsman');
-define('EMAIL_OMBUDSMAN', 'ombudsman@ombudsman.com');
+define('SEND_EMAILS', getenv('OMBUD_SEND_EMAILS'));
+define('SMTP_HOST', getenv('OMBUD_SMTP_HOST');
+define('SMTP_PORT', getenv('OMBUD_SMTP_PORT');
+define('SMTP_USER', getenv('OMBUD_SMTP_USER');
+define('SMTP_PASS', getenv('OMBUD_SMTP_PASS');
+define('SMTP_USERNAME', getenv('OMBUD_SMTP_USERNAME'));
+define('EMAIL_OMBUDSMAN', getenv('OMBUD_EMAIL_OMBUDSMAN'));
 
 define('HEADER_TEMPLATE', ABSPATH . '../inc/header.php');
 define('FOOTER_TEMPLATE', ABSPATH . '../inc/footer.php');
 
-define('LANGUAGE', 'EN'); // EN or PT_BR
-define('TIMEZONE', 'America/Fortaleza');
-define('DATEFORMAT', 'd/m/Y H:i:s');
+define('LANGUAGE', getenv('OMBUD_LANGUAGE')); // EN or PT_BR
+define('TIMEZONE', getenv('OMBUD_TIMEZONE');
+define('DATEFORMAT', getenv('OMBUD_DATEFORMAT');
 
-define('PAGINATOR_LIMIT', 8);
-define('PAGINATOR_LINKS', 5);
+define('PAGINATOR_LIMIT', getenv('OMBUD_PAGINATOR_LIMIT'));
+define('PAGINATOR_LINKS', getenv('OMBUD_PAGINATOR_LINKS'));
 
-define('DEBUG', true);
+define('DEBUG', getenv('OMBUD_DEBUG'));
 ?>
