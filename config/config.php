c0f0076d757a6ce028cbf69561e7ad5dd7571c13
Murilo Holanda <murilolh@gmail.com>
2018-11-27 20:27:43 -0200

    Changing config.php for Heroku deploy

--- config/config.php
+++ config/config.php
@@ -9,7 +9,7 @@ if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
 if ( !defined('BASEURL') )
-	define('BASEURL', '/');
+	define('BASEURL', getenv('OMBUD_BASE_URL'));
 
 if ( !defined('SYSTEMNAME') )
 	define('SYSTEMNAME', 'Ombudsman');
@@ -17,20 +17,20 @@ if ( !defined('SYSTEMNAME') )
 if ( !defined('DBAPI') )
 	define('DBAPI', ABSPATH . '../inc/database.php');
 
-	define('SEND_EMAILS', getenv('OMBUD_SEND_EMAILS'));
-	define('SMTP_HOST', getenv('OMBUD_SMTP_HOST');
-	define('SMTP_PORT', getenv('OMBUD_SMTP_PORT');
-	define('SMTP_USER', getenv('OMBUD_SMTP_USER');
-	define('SMTP_PASS', getenv('OMBUD_SMTP_PASS');
-	define('SMTP_USERNAME', getenv('OMBUD_SMTP_USERNAME'));
-	define('EMAIL_OMBUDSMAN', getenv('OMBUD_EMAIL_OMBUDSMAN'));
+define('SEND_EMAILS', getenv('OMBUD_SEND_EMAILS'));
+define('SMTP_HOST', getenv('OMBUD_SMTP_HOST');
+define('SMTP_PORT', getenv('OMBUD_SMTP_PORT');
+define('SMTP_USER', getenv('OMBUD_SMTP_USER');
+define('SMTP_PASS', getenv('OMBUD_SMTP_PASS');
+define('SMTP_USERNAME', getenv('OMBUD_SMTP_USERNAME'));
+define('EMAIL_OMBUDSMAN', getenv('OMBUD_EMAIL_OMBUDSMAN'));
 
 define('HEADER_TEMPLATE', ABSPATH . '../inc/header.php');
 define('FOOTER_TEMPLATE', ABSPATH . '../inc/footer.php');
 
 define('LANGUAGE', getenv('OMBUD_LANGUAGE')); // EN or PT_BR
-define('TIMEZONE', getenv('OMBUD_TIMEZONE');
-define('DATEFORMAT', getenv('OMBUD_DATEFORMAT');
+define('TIMEZONE', getenv('OMBUD_TIMEZONE'));
+define('DATEFORMAT', getenv('OMBUD_DATEFORMAT'));
 
 define('PAGINATOR_LIMIT', getenv('OMBUD_PAGINATOR_LIMIT'));
 define('PAGINATOR_LINKS', getenv('OMBUD_PAGINATOR_LINKS'));
